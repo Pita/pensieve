08c32924d85d242d80cb4dfdafebbbf4178db3ae
Peter 'Pita' Martischka <peter.martischka@produktmacher.com>
2019-10-19 12:58:26 -0300

    rewrote startSession with middy

--- backend/src/startSession.ts
+++ backend/src/startSession.ts
@@ -1,49 +1,74 @@
-// Helper
-import { success, clientError } from "./response";
-import { APIGatewayEvent, Handler } from "aws-lambda";
+import middy from "middy";
+import { jsonBodyParser, validator, httpErrorHandler } from "middy/middlewares";
 import speakeasy from "speakeasy";
 import secrets from "./secrets.json";
 import jwt from "jsonwebtoken";
 
 function generateId() {
   const length = 8;
-  let result             = '';
-  const characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   const charactersLength = characters.length;
-  for ( var i = 0; i < length; i++ ) {
-     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
   return result;
 }
 
-function getOTP(event) {
-  const { body } = event;
-  const parsedBody = JSON.parse(body);
-  return parsedBody.otp as string;
-}
+const startSession = async event => {
+  const { otp } = event.body;
 
-export const handler: Handler<APIGatewayEvent> = async (event: APIGatewayEvent) => {
-  let otp;
-  try {
-    otp = getOTP(event);
-  } catch(e) {
-    return clientError({message: "error parsing otp"});
-  }
-
-  const verified = speakeasy.totp.verify({ 
+  const verified = speakeasy.totp.verify({
     secret: secrets.otp.secret,
-    encoding: 'base32',
-    token: otp 
+    encoding: "base32",
+    token: otp
   });
 
   if (!verified) {
-    return clientError({message: "invalid otp"});
+    // TODO: Better error handling
+    return {
+      statusCode: 400,
+      headers: {},
+      body: JSON.stringify({ status: "error_invalid_otp" })
+    };
   }
 
-  const token = jwt.sign({
-    id: generateId(),
-    exp: Math.floor(Date.now() / 1000) + (60 * 60),
-  }, secrets.jwt.private, { algorithm: 'RS256'});
+  const token = jwt.sign(
+    {
+      id: generateId(),
+      exp: Math.floor(Date.now() / 1000) + 60 * 60
+    },
+    secrets.jwt.private,
+    { algorithm: "RS256" }
+  );
 
-  return success({token, entrySecrets: secrets.entry})
+  return {
+    statusCode: 200,
+    headers: {},
+    body: JSON.stringify({ status: "ok", token, entrySecrets: secrets.entry })
+  };
 };
+
+const inputSchema = {
+  type: "object",
+  properties: {
+    body: {
+      type: "object",
+      properties: {
+        otp: {
+          type: "string",
+          pattern: "^\\d{6}$"
+        }
+      },
+      required: ["otp"]
+    }
+  }
+};
+
+export const handler = middy(startSession)
+  // TODO: should probably use @middy/function-shield
+  // TODO: cors
+  .use(jsonBodyParser())
+  .use(validator({ inputSchema }))
+  .use(httpErrorHandler());
